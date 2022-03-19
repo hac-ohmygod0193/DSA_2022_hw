e673ed76cf116eb6d141cd93c98bc7be844e29c7
ohmygod0193 <62164142+hac-ohmygod0193@users.noreply.github.com>
2022-03-19 10:05:41 +0800

    Update Problem 4 - Calculate Simple Integers Easily.c

--- hw1/Problem 4 - Calculate Simple Integers Easily.c
+++ hw1/Problem 4 - Calculate Simple Integers Easily.c
@@ -1,62 +1,51 @@
 #include<stdio.h>
+#include <assert.h>
+// ((1+2)*5+(1+7+11))%5-11+
+// let '-'=-1, '+'=1 ,'*'=2 ,'/'=-2, '('=-3, ')'=3;
 #define int long long
-int char_to_int(char a){
-    if(a=='-') return -1;
-    else if(a=='+') return 1;
-    else if(a=='%') return 2;
-    else if(a=='*') return 3;
-    else if(a=='/') return -3;
-    else if(a=='(') return -4;
-    else if(a==')') return 4;
+int priority(char a){
+    if(a=='+' || a=='-') return -1;
+    else return 1;
 }
-char int_to_char(int a){
-    if(a==-1) return '-';
-    else if(a==1) return '+';
-    else if(a==2) return '%';
-    else if(a==-3) return '/';
-    else if(a==3) return '*';
-    else if(a==-4) return '(';
-    else if(a==4) return ')';
-}
-typedef struct str{
-    int len;
-    char c[100];
-} STR;
-
 typedef struct element{
     int is_op;
-    int num,op;
+    int num;
+    char op;
 } math;
-typedef struct a{
-    math x[100];
-} formula;
-formula infix_to_postfix(STR s){
-    formula ans;
-    int op[100];
-    int cur,top,sum=0,num;
+char s[1000001];
+signed main(){
+    scanf("%s",s);
+    int len = strlen(s);
+    //printf("%d\n",len);
+    math *ans;
+    ans = malloc(len*sizeof(math));
+    char *op;
+    op = malloc(len*sizeof(char));
+    char cur,top;
+    int sum=0,num;
     int opp=-1,k=0;//opp->tail pos
     //printf("%d\n",strlen(s.c));
     int negative=0;
     int is_number=0;
-    for(int i=0;i<s.len-1;i++){
-        num = s.c[i]-'0';
+    for(int i=0;i<len-1;i++){
+        num = s[i]-'0';
         if(num>=0 && num<=9){
             //printf("haha\n");
             is_number=1;
             sum*=10;
-            if(negative==1) sum-=num;
-            else sum+=num;
+            //if(negative==1) sum-=num;
+            sum+=num;
         }
         else{
             if(is_number==1){
-                ans.x[k].is_op=0;
-                ans.x[k].num=sum;
+                ans[k].is_op=0;
+                ans[k].num=sum;
                 k++;
                 //printf("sum=%d, %c\n",sum,s.c[i]);
                 sum=0;
                 is_number=0;
             }
-            cur = char_to_int(s.c[i]);
+            cur = s[i];
             //printf("char=%d\n",cur);
             if(opp==-1){
                 opp++;
@@ -65,24 +54,28 @@ formula infix_to_postfix(STR s){
             }
             else{
                 top = op[opp];
-                if(cur==4){//')'
+                if(cur==')'){//')'
                     //printf("%c\n",s[i]);
-                    while(top!=-4 && opp>=0){
-                        ans.x[k].is_op=1;
-                        ans.x[k].op=top;
+                    while(top!='(' && opp>=0){
+                        ans[k].is_op=1;
+                        ans[k].op=top;
                         k++;
                         opp--;
                         if(opp==-1) break;
                         top = op[opp];
                     }
-                    if(top==-4){
+                    if(top=='('){
                         opp--;
                     }
                 }
+                else if(cur=='('){
+                    opp++;
+                    op[opp]=cur;
+                }
                 else{
-                    while(abs(top)>=abs(cur) && opp>=0 && top!=-4){
-                        ans.x[k].is_op=1;
-                        ans.x[k].op=top;
+                    while(top!='(' && priority(top)>=priority(cur) && opp>=0){
+                        ans[k].is_op=1;
+                        ans[k].op=top;
                         k++;
                         opp--;
                         if(opp==-1) break;
@@ -98,68 +91,60 @@ formula infix_to_postfix(STR s){
 
     }
     if(is_number==1){
-        ans.x[k].is_op=0;
-        ans.x[k].num=sum;
+        ans[k].is_op=0;
+        ans[k].num=sum;
         k++;
         sum=0;
         is_number=0;
     }
     for(int i=opp;i>=0;i--){
-        ans.x[k].is_op=1;
-        ans.x[k].op=op[i];
+        ans[k].is_op=1;
+        ans[k].op=op[i];
         k++;
     }
-    ans.x[k].is_op=-1;
-    return ans;
-}
-int cal(formula ans){
-    int st[100];
-    int ptr=-1,a,b,num;
-    char op;
-    for(int i=0;i<100;i++){
-        if(ans.x[i].is_op==-1) break;
-        if(ans.x[i].is_op==0){
+    ans[k].is_op=-1;
+    free(op);
+
+    //calculate
+    int ptr=-1,a,b;
+    char opt;
+    int *st;
+    st = malloc(len*sizeof(int));
+    for(int i=0;i<len;i++){
+        if(ans[i].is_op==-1) break;
+        if(ans[i].is_op==0){
             ptr++;
-            st[ptr] = ans.x[i].num;
+            st[ptr] = ans[i].num;
         }
         else{
             b = st[ptr];
             ptr--;
             a = st[ptr];
             //printf("a = %d, b = %d, ans=",a,b);
-            op = int_to_char(ans.x[i].op);
-            if(op=='+'){
+            opt = ans[i].op;
+            if(opt=='+'){
                 st[ptr] = a+b;
             }
-            else if(op=='-'){
+            else if(opt=='-'){
                 st[ptr] = a-b;
             }
-            else if(op=='*'){
+            else if(opt=='*'){
                 st[ptr] = a*b;
             }
-            else if(op=='/'){
+            else if(opt=='/'){
                 int c = a/b;
                 if(c<=0 && c*b>a){
                     c=c-1;
                 }
-                //printf("%d %d",a,b);
                 st[ptr] = c;
             }
-            else if(op=='%'){
+            else if(opt=='%'){
                 st[ptr] = a%b;
             }
             //printf("%d\n",st[ptr]);
         }
     }
-    //printf("result=%d\n",st[ptr]);
-    return st[ptr];
-}
-signed main(){
-    STR s;
-    formula ans;
-    //printf("%d\n",1+6/3%4);
-    scanf("%s",s.c);
-    s.len = strlen(s.c);
-    ans = infix_to_postfix(s);
-    printf("Print: %d\n",cal(ans));
+    printf("Print: %d\n",st[ptr]);
+    free(st);
+    free(ans);
 }
