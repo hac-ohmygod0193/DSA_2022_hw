b47af72c843a5e6dc7c778d5ce2c886af378dc0b
ohmygod0193 <62164142+hac-ohmygod0193@users.noreply.github.com>
2022-03-20 13:17:54 +0800

    Update Problem 4 - Calculate Simple Integers Easily.c

--- hw1/Problem 4 - Calculate Simple Integers Easily.c
+++ hw1/Problem 4 - Calculate Simple Integers Easily.c
@@ -10,55 +10,44 @@ int priority(char a){
 typedef struct element{
     int is_op;
     int num;
-    char op;
+    char opt;
 } math;
-char s[1000001];
-signed main(){
-    scanf("%s",s);
-    int len = strlen(s);
-    //printf("%d\n",len);
-    math *ans;
-    ans = malloc(len*sizeof(math));
+void inToPostfix(char* s,math* ans,int len,int k){
     char *op;
     op = malloc(len*sizeof(char));
     char cur,top;
     int sum=0,num;
-    int opp=-1,k=0;//opp->tail pos
-    //printf("%d\n",strlen(s.c));
+    int opp=-1;
     int negative=0;
     int is_number=0;
-    for(int i=0;i<len-1;i++){
+
+    for(int i=0;i<len;i++){
         num = s[i]-'0';
         if(num>=0 && num<=9){
-            //printf("haha\n");
             is_number=1;
             sum*=10;
-            //if(negative==1) sum-=num;
-            sum+=num;
+            if(negative==1) sum-=num;
+            else sum+=num;
         }
         else{
             if(is_number==1){
                 ans[k].is_op=0;
                 ans[k].num=sum;
                 k++;
-                //printf("sum=%d, %c\n",sum,s.c[i]);
                 sum=0;
                 is_number=0;
             }
             cur = s[i];
-            //printf("char=%d\n",cur);
             if(opp==-1){
                 opp++;
                 op[opp]=cur;
-                //printf("opp=%d\n",op[opp]);
             }
             else{
                 top = op[opp];
                 if(cur==')'){//')'
-                    //printf("%c\n",s[i]);
                     while(top!='(' && opp>=0){
                         ans[k].is_op=1;
-                        ans[k].op=top;
+                        ans[k].opt=top;
                         k++;
                         opp--;
                         if(opp==-1) break;
@@ -73,9 +62,9 @@ signed main(){
                     op[opp]=cur;
                 }
                 else{
-                    while(top!='(' && priority(top)>=priority(cur) && opp>=0){
+                    while(priority(top)>=priority(cur) && opp>=0 && top!='('){
                         ans[k].is_op=1;
-                        ans[k].op=top;
+                        ans[k].opt=top;
                         k++;
                         opp--;
                         if(opp==-1) break;
@@ -87,8 +76,6 @@ signed main(){
 
             }
         }
-
-
     }
     if(is_number==1){
         ans[k].is_op=0;
@@ -99,18 +86,18 @@ signed main(){
     }
     for(int i=opp;i>=0;i--){
         ans[k].is_op=1;
-        ans[k].op=op[i];
+        ans[k].opt=op[i];
         k++;
     }
     ans[k].is_op=-1;
     free(op);
-
-    //calculate
-    int ptr=-1,a,b;
-    char opt;
+}
+int cal(math* ans,int len){
     int *st;
     st = malloc(len*sizeof(int));
-    for(int i=0;i<len;i++){
+    int ptr=-1,a,b;
+    char opt;
+    for(int i=0;i<len+1;i++){
         if(ans[i].is_op==-1) break;
         if(ans[i].is_op==0){
             ptr++;
@@ -120,8 +107,7 @@ signed main(){
             b = st[ptr];
             ptr--;
             a = st[ptr];
-            //printf("a = %d, b = %d, ans=",a,b);
-            opt = ans[i].op;
+            opt = ans[i].opt;
             if(opt=='+'){
                 st[ptr] = a+b;
             }
@@ -133,7 +119,7 @@ signed main(){
             }
             else if(opt=='/'){
                 int c = a/b;
-                if(c<=0 && c*b>a){
+                if(!(a>0 && b>0) && a%b){
                     c=c-1;
                 }
                 st[ptr] = c;
@@ -141,10 +127,43 @@ signed main(){
             else if(opt=='%'){
                 st[ptr] = a%b;
             }
-            //printf("%d\n",st[ptr]);
+
         }
     }
-    printf("Print: %d\n",st[ptr]);
+    return st[0];
     free(st);
-    free(ans);
+}
+
+char s[1000001];
+char c[1000001];
+signed main(){
+    scanf("%s",c);
+    int len;
+    int p=0,result=0;
+    int first=0;
+    //printf("%d\n",len);
+    for(int j=0;j<strlen(c);j++){
+        if(c[j]!='='){
+            s[p]=c[j];p++;
+            continue;
+        }
+        else{
+            s[p]='\0';
+            len = p;
+            p=0;
+        }
+        math *ans;
+        ans = malloc((len+1)*sizeof(math));
+        int k=0;
+        if(first==1){
+            ans[k].is_op=0;
+            ans[k].num=result;
+            k++;
+        }
+        inToPostfix(s,ans,len,k);
+        result = cal(ans,len);
+        printf("Print: %d\n",result);
+        free(ans);
+        first=1;
+    }
 }
